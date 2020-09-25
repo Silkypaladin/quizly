ae9e603ec2acfb0388db6c4c499fa62ca3b56335
Silkypaladin <bednarski.szymon99@gmail.com>
2020-09-25 19:19:15 +0200

    Write register form, update login css styling
    Write html template for register component, update error messages,
    showing on incorrect input, prepare validation for registration process

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -9,17 +9,39 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class LoginComponent implements OnInit {
 
   loginForm: FormGroup;
-  loading: boolean;
+  loading: boolean = false;
+  submitted: boolean = false;
 
-  constructor(
-    private formBuilder: FormBuilder,
-  ) { }
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
     this.loginForm = this.formBuilder.group({
       username: ['', Validators.required],
-      password: ['', Validators.required]
+      password: ['', [Validators.required, Validators.minLength(6)]]
     });
+  }
+
+  input() {
+    return this.loginForm.controls;
+  }
+
+  length() {
+    let obj = this.loginForm.controls['password'].errors.minlength;
+    if (obj === undefined) {
+      return false
+    }
+    return obj['requiredLength'] > obj['actualLength']
+  }
+  onSubmit() {
+    this.submitted = true;
+    if (this.loginForm.invalid) {
+      return;
+  }
+
+  this.loading = true;
+
+  // display form values on success
+  alert('SUCCESS!! :-)\n\n' + JSON.stringify(this.loginForm.value, null, 4));
   }
 
 }
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +8,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  loading: boolean = false;
+  submitted: boolean = false;
+  registerForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    this.registerForm = this.formBuilder.group({
+      username: ['', Validators.required],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      passwordRepeat: ['', [Validators.required, Validators.minLength(6)]],
+      email: ['', [Validators.required, Validators.email]],
+    });
   }
 
+  input() {
+    return this.registerForm.controls;
+  }
+
+  length() {
+    let obj = this.registerForm.controls['password'].errors.minlength;
+    if (obj === undefined) {
+      return false
+    }
+    return obj['requiredLength'] > obj['actualLength']
+  }
+
+  passwordsNotMatching() {
+    let pass = this.registerForm.controls['password'].value;
+    let passRepeat = this.registerForm.controls['passwordRepeat'].value;
+    return pass !== passRepeat
+  }
+
+  onSubmit() {
+    this.submitted = true;
+    if (this.registerForm.invalid) {
+      return;
+  }
+
+  this.loading = true;
+
+  // display form values on success
+  alert('SUCCESS!! :-)\n\n' + JSON.stringify(this.registerForm.value, null, 4));
+  }
 }
